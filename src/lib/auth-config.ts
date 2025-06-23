2c0efdead1e209b949408e41c95761c624bb4965
Marvellousz <pranavmurali024@gmail.com>
2025-06-23 15:42:38 +0530

    fix: fixed signin issue

--- src/lib/auth-config.ts
+++ src/lib/auth-config.ts
@@ -54,7 +54,6 @@ export const authConfig: NextAuthConfig = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
       authorization: {
         params: {
-          prompt: "consent",
           access_type: "offline",
           response_type: "code",
           scope: "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.events https://mail.google.com/",
